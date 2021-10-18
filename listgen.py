befa788cdf191bc7a105256a9e68b01ce7d7c3e2
Enric Carstens <enric.carstens@gmail.com>
2021-10-18 13:38:07 +0200

    initial

---
+++ listgen.py
@@ -0,0 +1,33 @@
+import os
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Settings')
+    parser.add_argument('From', metavar='<from>', type=int, help='Starting point')
+    parser.add_argument('To', metavar='<to>', type=int, help='Ending point')
+    parser.add_argument('Step', metavar='<step>', type=int, help='Step')
+
+    args = parser.parse_args()
+
+    numFrom = args.From
+    numTo = args.To
+    numStep = args.Step
+
+    filename = f'nums_{numFrom}_{numTo}_{numStep}.txt'
+
+    # print(numFrom, numTo, numStep)
+
+    if not os.path.exists('./lists'):
+        os.makedirs('./lists')
+
+    with open(f'lists/{filename}', 'w') as outfile:
+        for i in range(numFrom, numTo + 1, numStep):
+            outfile.write(str(i) + '\n')
+        
+
+    print(f'List successfully created under lists/{filename}')
+
+
+if __name__ == "__main__":
+    main()
